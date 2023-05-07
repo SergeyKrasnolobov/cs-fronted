85a8166454cb514f5345df3a348c7bb798379f18
Краснолобов Сергей <SKrasnolobov@beeline.ru>
2023-05-07 21:26:09 +0300

    Доработал коллапс для 5 урока используя очередь

--- src/lesson-5/collapse.ts
+++ src/lesson-5/collapse.ts
@@ -1,4 +1,4 @@
-export const collapse = (obj: Record<string, any>) => {
+export const collapseRecursive = (obj: Record<string, any>) => {
   const result: Record<string, any> = {};
 
   function traverse(obj: Record<string, any>, flatKey = "") {
@@ -30,4 +30,25 @@ const obj = {
   },
 };
 
-collapse(obj);
+export const collapseOnQueue = (obj: Record<string, any>) => {
+  const result: Record<string, any> = {};
+  const stack: any[] = Object.entries(obj);
+
+  const createKey = (key: string, prefix: string) => (prefix ? `${prefix}.${key}` : key);
+
+  while (stack.length) {
+    const [key, value] = stack.pop();
+    if (typeof value !== "object") {
+      result[key] = value;
+    }
+    const initialKey = createKey(key, "");
+    const entries = Object.entries(value);
+    for (const [key, _value] of entries) {
+      stack.unshift([createKey(key, initialKey), _value]);
+    }
+  }
+  return result;
+};
+
+collapseRecursive(obj);
+collapseOnQueue(obj);
