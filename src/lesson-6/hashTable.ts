bbd6206857ebab07ffdb1f468b9c192a1a83d0ee
Краснолобов Сергей <SKrasnolobov@beeline.ru>
2023-05-12 18:27:15 +0300

    Реализайия хеш-таблицы

--- src/lesson-6/hashTable.ts
+++ src/lesson-6/hashTable.ts
@@ -12,9 +12,9 @@ type CalculateHash = (key: number | string | object) => number;
 
 /**
  *
- * Для взятия хеша у объекта рсширяем протоитип объекта 2 функциями
+ * Для взятия хеша у объекта расширяем его прототип  функцией getHashCode
  *
- * getHashCode(){} создает хеш код и записвает его в свойства объекта
+ * getHashCode(){} создает хеш код и записывает его в свойства объекта
  * hashCode: геттер вернет хеш код
  */
 
@@ -65,7 +65,7 @@ class HashTable<K extends number | string | object, V> {
   #size = 0;
   hasher: Hasher;
   capacity: number;
-  #loadIndex = 0.75;
+  #loadRate = 0.75;
   constructor(hasher: Hasher, capacity: number = 13) {
     this.hasher = hasher;
     this.capacity = capacity;
@@ -77,6 +77,9 @@ class HashTable<K extends number | string | object, V> {
   }
 
   set(key: string | number | object, value: any) {
+    if (this.#isNeedResize()) {
+      this.#resize();
+    }
     const node = new ListNode(key, value) as ListNode<K, V>;
     const index = this.#getIndex(this.hasher.getHash(key));
 
@@ -91,7 +94,7 @@ class HashTable<K extends number | string | object, V> {
           currentNode.value = value;
           return;
         } else {
-          //если у текущей ноды нет next Node, то присваем ей в next новую ноду, и выходим из цикла
+          //если у текущей ноды нет next Node, то присваиваем ей в next новую ноду, и выходим из цикла
           if (!currentNode.next) {
             currentNode.next = node;
             break;
@@ -102,28 +105,96 @@ class HashTable<K extends number | string | object, V> {
     }
     this.#size++;
   }
+  has(key: string | number | object) {
+    const index = this.#getIndex(this.hasher.getHash(key));
+
+    let node = this.#buffer[index];
+    if (!node) return undefined;
+    while (node) {
+      if (node.key === key) {
+        return true;
+      }
+      node = node.next as any;
+    }
+    return false;
+  }
+  get(key: string | number | object) {
+    const index = this.#getIndex(this.hasher.getHash(key));
+
+    let node = this.#buffer[index];
+    if (!node) return undefined;
+    while (node) {
+      if (node.key === key) {
+        return node.value;
+      }
+      node = node.next as any;
+    }
+  }
+
+  #isNeedResize() {
+    return this.#size / this.#buffer.length >= this.#loadRate;
+  }
+  #resize() {
+    const newCapacity = this.#getNextPrime(this.#buffer.length * 2);
+    const oldBuffer = this.#buffer;
+    this.#buffer = new (this.#buffer.constructor as ArrayConstructor)(newCapacity);
+    console.log({ size: this.#size, oldL: oldBuffer.length, newL: this.#buffer.length });
+
+    // чтобы использовать метод set, сбрасываем кол-во элементов
+    this.#size = 0;
+
+    for (let i = 0; i < oldBuffer.length; i++) {
+      let node = oldBuffer[i];
+      if (!node) {
+        continue;
+      } else {
+        while (node) {
+          this.set(node.key, node.value);
+          node = node.next as any;
+        }
+      }
+    }
+  }
+  #getNextPrime(doubledOriginBufferLength: number) {
+    let number = doubledOriginBufferLength;
+
+    while (true) {
+      if (this.#isPrime(number)) {
+        return number;
+      }
+      number++;
+    }
+  }
+
+  #isPrime(num: number) {
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
 
   log() {
-    console.log(this.#buffer, { loadIndex: this.#size / this.#buffer.length });
+    console.log(this.#buffer, this.#size, { loadIndex: this.#size / this.#buffer.length });
   }
 }
 
 const hasher = new Hasher();
 const hashMap = new HashTable(hasher);
 
-// hashMap.set("foo", 23);
+hashMap.set("foo", 23);
 hashMap.set("oof", 232); //potential collision
-// hashMap.set("bar", 24);
-// hashMap.set("cat", 25);
-// hashMap.set("fat", 25);
-// hashMap.set("jhon", 25);
-// hashMap.set("malkovich", 25);
-// hashMap.set("r", 25);
-hashMap.set({}, 25);
-hashMap.set({}, 25);
+hashMap.set("bar", 24);
+hashMap.set("cat", 25);
+hashMap.set("fat", 25);
+hashMap.set("jhon", 25);
+hashMap.set("malkovich", 25);
+hashMap.set("r", 25);
 hashMap.set({}, 25);
 
-// hashMap.set("fat", 255);
-// hashMap.set(0, 255);
+hashMap.set("fat", 255);
 
-hashMap.log();
+hashMap.set(0, 255);
+
+console.log(hashMap.get("oof")); // 232
+console.log(hashMap.has("oof")); // true
+console.log(hashMap.has("oof2")); // false
